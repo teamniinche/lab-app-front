6c5e9fb902c03e0fd732de731ab8f7a56c7b0627
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2026-08-21 15:35:14 +0200

    Fix SOCKET_SERVER_URL to include '/api' suffix

--- assets/socketService.js
+++ assets/socketService.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 import { dbBaseRoot } from './constantes';
 
 // Correction de la logique d'URL
-const SOCKET_SERVER_URL = dbBaseRoot.split('/tn-api')[0];
+const SOCKET_SERVER_URL = dbBaseRoot.split('/tn-api')[0]+'/api';
 const socket = io(SOCKET_SERVER_URL, { // Toujours utiliser l'URL du serveur API
   transports: ["websocket"],           // Doit matcher le serveur
   autoConnect: true,
