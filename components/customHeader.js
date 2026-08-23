3590f91c21acdf40cd37ac587b96e139b16af964
msn numerique <ndourm9@gmail.com>
2026-08-23 03:47:46 +0200

    'commitauto'

--- components/customHeader.js
+++ components/customHeader.js
@@ -39,8 +39,8 @@ const Entete=({thisEntete,donnees,render})=>{
     return (
       <MaterialCommunityIcons 
         name={directionTri === 'asc' ? 'arrow-up' : 'arrow-down'} 
-        size={12} 
-        color="#ffffff" 
+        size={14} 
+        color="#222222" 
         style={styles.icone}
       />
     );
@@ -54,7 +54,7 @@ const Entete=({thisEntete,donnees,render})=>{
 
 const styles = StyleSheet.create({
     enTeteCell: { flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' },
-    enTeteTexte: { fontWeight: 'bold', fontSize: 14 },
+    enTeteTexte: { fontWeight: 'bold', fontSize: 10 },
     icone: { marginLeft: 4 },
 });
 
