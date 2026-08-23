ef9da9da4d9ddae46ac6ecaaf07cb57c2c364d4d
msn numerique <ndourm9@gmail.com>
2026-08-23 15:29:09 +0200

    'commitauto'

--- App.js
+++ App.js
@@ -295,7 +295,7 @@ export const ToggleTheme=()=>{
     }
 
 
-return <TouchableOpacity style={{position:'absolute',right:25,top:10,backgroundColor:THEME.backgroundClr,padding:8,paddingHorizontal:24,borderRadius:8,}}  onPress={handleThemeChange}>
+return <TouchableOpacity style={{position:'absolute',right:20,top:8,backgroundColor:THEME.backgroundClr,padding:8,paddingHorizontal:24,borderRadius:8,}}  onPress={handleThemeChange}>
           <Text style={{color:'transparent',}}>
             <FontAwesome name={THEME.icone} size={15} color={THEME.iconeColor}/>
           </Text>
