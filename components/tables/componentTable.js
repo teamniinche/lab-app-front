f990075a585a8ca3580d730755af950951a10f96
msn numerique <ndourm9@gmail.com>
2026-08-24 02:51:30 +0200

    'commitauto'

--- components/tables/componentTable.js
+++ components/tables/componentTable.js
@@ -232,7 +232,7 @@ return (<HideStatusBarOnFocus>
                   if(interDate.minDate===""){
                     // ()=>setInterDate({...interDate,minDate:ReduceDateTime(createdAt)}); // A VOIR A REVOIR  A VOIR A REVOIR
                   }
-                return !nteteIsIn(realKeyFromEntete(entete,item))?<EnteteRow ntte={realKeyFromEntete(entete,item)}/>:
+                return !enteteIsIn(realKeyFromEntete(entete,item))?<EnteteRow ntte={realKeyFromEntete(entete,item)}/>:
                       <ClickableRow
                           key={index}
                           num={index+1}
