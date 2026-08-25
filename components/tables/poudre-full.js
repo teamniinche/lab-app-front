862b59dd62b76529f41e807331742d3a226f7b8f
msn numerique <ndourm9@gmail.com>
2026-08-25 21:04:47 +0200

    'commitauto'

--- components/tables/poudre-full.js
+++ components/tables/poudre-full.js
@@ -101,7 +101,7 @@ export default PoudreFull=({navigation/*,route*/,routeNheaders})=>{
                             const {id,updatedAt,createdAt,identifier,type,categorie,taches,...rest} = it;
                             const valeurs=['nChar','humidite','matiere_active','alcanite','gg','silicate','sel','densite'];
                             const vallues = VALEURSPOUDRE(rest,valeurs);
-                            return `<p style="margin-left:5px;color:'grey';font-size:${Platform.OS!=='web'?'14px':'15px'};letter-spacing:0.7px;border-bottom:${Platform.OS==='web' && `1px solid grey`};">
+                            return `<p style="margin-left:5px;color:'grey';font-size:${Platform.OS!=='web'?'12px':'15px'};text-wrap:nowrap;text-overflow: ellipsis;letter-spacing:0.7px;border-bottom:${Platform.OS==='web' && `1px solid grey`};">
                                         ${Time(createdAt)}${vallues}
                                     </p>`;
                             }).join("")}
