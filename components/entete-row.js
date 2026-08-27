5e6016ddde72bff3722cc4b71ccf161372bba49e
msn numerique <ndourm9@gmail.com>
2026-08-27 17:42:51 +0200

    'commitauto'

--- components/entete-row.js
+++ components/entete-row.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet,useState,useLayoutEffect,Pressable,Platform} from 'react-native';
+import {useState,useLayoutEffect} from 'react';
+import { View, Text, StyleSheet,Pressable,Platform} from 'react-native';
 import { useSelector} from 'react-redux';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import * as Sharing from 'expo-sharing';
