6a60b3aee96f49396bdd67eafd0e75ed5888f56f
msn numerique <ndourm9@gmail.com>
2026-09-01 20:23:30 +0200

    'commitauto'

--- components/entete-row.js
+++ components/entete-row.js
@@ -219,7 +219,7 @@ export const EnteteRow=({item,ntte,prodName,analysed})=>{
                 return `<h3 class="key3">${key}<span style="display:inline-block;background-color:blue;font-weight:bold;font-size:14px;border-radius:50%;width:auto;height:auto;padding:4px;margin-left:5px;margin-right:5px;color:white;border:1px solid grey">${count}</span><h3>
                     <h3 class="key3">${' Moyennes '+rest.map(r=>{
                     const moyR=moy(analysedR,r);
-                    return (moyR!=="" && enetete==='name')?`<span style="margin:15px;">${keyReduce(r)+': ' +moyR/*moy(Object.values(object),r)*/}</span>`:null})}<h3>
+                    return (moyR!=="" && entete==='name')?`<span style="margin:15px;">${keyReduce(r)+': ' +moyR/*moy(Object.values(object),r)*/}</span>`:null})}<h3>
                 
                 </div>
                         ${Object.entries(object).map(([k,item]) => {
