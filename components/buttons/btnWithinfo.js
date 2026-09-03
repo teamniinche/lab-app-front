eb78d8c938eb81f1351adab528d62a262b457c26
msn numerique <ndourm9@gmail.com>
2026-09-03 15:20:05 +0200

    'commitauto'

---
+++ components/buttons/btnWithinfo.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import Tooltip from 'react-native-walkthrough-tooltip';
+
+export default function Btn(props) {
+  const {style,onPress,info,children,...restProps}=props;
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  return (
+    <Tooltip
+      isVisible={showTooltip}
+      content={<Text style={{backgroundColor:'rgba(0,0,0,0.8)',color:'white',height:40,padding:5,borderRadius:8,textAlign:'center',fontWeight:'bold',fontSize:12}}>info</Text>}
+      placement="top"
+      onClose={() => setShowTooltip(false)}
+    >
+      <TouchableOpacity 
+        onPress={onPress}
+        {...restProps}
+        onHoverIn={() => setShowTooltip(true)}
+        onHoverOut={() => setShowTooltip(false)}
+        style={style}
+      >
+        {children}
+      </TouchableOpacity>
+    </Tooltip>
+  );
+}
--- components/tables/table-full.js
+++ components/tables/table-full.js
@@ -17,6 +17,7 @@ import { MyChip,Filter } from '../chip';
 import { Periodes } from '../periode';
 import { clooner } from '../accueil';
 import Table from './componentTable'
+import Btn from '../buttons/btnWithinfo.js';
 import Pop from '../popup';
 // DATA STATEMENTS && UTILS
 import { ChipProvider,CurrentProductsProvider,useEverages,usePopup,usePowderEverages,useReservs } from '../wrappers/contexts';
@@ -493,12 +494,13 @@ export default TableFull=({navigation/*,route*/,routeNheaders})=>{
             </SafeAreaProvider>
 
             <View style={styles.print_graphes}>
-                <Pressable style={styles.graphes_link} onPress={()=>navigation.navigate("analyses/liquides/graphes")}>
+                {/* const {style,onPress,info,children,...restProps}=props; */}
+                <Btn style={styles.graphes_link} onPress={()=>navigation.navigate("analyses/liquides/graphes")} info="diagrammes">
                     <FontAwesome5 size={25} name="chart-line" color='blue'/>
-                </Pressable>
-                <TouchableOpacity style={styles.graphes_link} onPress={async () => {setTimeout(async () => {await generatePDF();}, 500);}}>
+                </Btn>
+                <Btn style={styles.graphes_link} onPress={async () => {setTimeout(async () => {await generatePDF();}, 500);}} info="imprimer table">
                     <Text ><FontAwesome5 name="file-pdf" size={25} color="blue"/></Text>
-                </TouchableOpacity>
+                </Btn>
             </View>
             
 
