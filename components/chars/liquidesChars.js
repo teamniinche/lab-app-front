0a1b324c852c690246a2094ef46e469beb19022d
msn numerique <ndourm9@gmail.com>
2026-09-04 12:24:31 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { /*LineChart,*/BarChart } from 'react-native-gifted-charts';
+import { LineChart,BarChart } from 'react-native-gifted-charts';
 
 // export default function Charts() {
 //   const data = [
