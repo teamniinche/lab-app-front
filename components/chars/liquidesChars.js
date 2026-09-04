2278b10b03f020cd6da89516d3061bed0a20072b
msn numerique <ndourm9@gmail.com>
2026-09-04 12:12:06 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -2,37 +2,37 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { /*LineChart,*/BarChart } from 'react-native-gifted-charts';
 
-export default function Charts() {
-  const data = [
-    { value: 40, label: 'Jan', frontColor: '#3498db' },
-    { value: 75, label: 'Fév', frontColor: '#3498db' },
-    { value: 60, label: 'Mar', frontColor: '#2ecc71' },
-    { value: 95, label: 'Avr', frontColor: '#2ecc71' },
-  ];
+// export default function Charts() {
+//   const data = [
+//     { value: 40, label: 'Jan', frontColor: '#3498db' },
+//     { value: 75, label: 'Fév', frontColor: '#3498db' },
+//     { value: 60, label: 'Mar', frontColor: '#2ecc71' },
+//     { value: 95, label: 'Avr', frontColor: '#2ecc71' },
+//   ];
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Rapport de Performance</Text>
-      {/* <View style={styles.card}>
-        <BarChart
-          data={data}
-          barWidth={35}
-          capRadius={4}
-          noOfSections={4}
-          maxValue={100}
-          yAxisTextStyle={{ color: '#7f8c8d' }}
-          xAxisLabelTextStyle={{ color: '#7f8c8d' }}
-        />
-      </View> */}
-      <View style={styles.card}>
-        <LChart/>
-      </View>
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Rapport de Performance</Text>
+//       {/* <View style={styles.card}>
+//         <BarChart
+//           data={data}
+//           barWidth={35}
+//           capRadius={4}
+//           noOfSections={4}
+//           maxValue={100}
+//           yAxisTextStyle={{ color: '#7f8c8d' }}
+//           xAxisLabelTextStyle={{ color: '#7f8c8d' }}
+//         />
+//       </View> */}
+//       <View style={styles.card}>
+//         <LChart/>
+//       </View>
+//     </View>
+//   );
+// }
 
 
-export  function LChart() {
+export default  function Charts() {
   // Données du graphique
   const data = [
     { value: 15, label: 'Lun' },
