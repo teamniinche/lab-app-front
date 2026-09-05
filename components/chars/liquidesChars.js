42332ed4f8766e0270abdf3a5655b48f6a3b34c4
msn numerique <ndourm9@gmail.com>
2026-09-05 11:05:54 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -165,7 +165,7 @@ export default function Charts(){
       color: '#ffffff',
       showArrowBase: true,
     }
-  })
+  }})
 
   return (<View style={styles.container}>
          <Text style={styles.title}>Statistiques Hebdomadaires</Text>
