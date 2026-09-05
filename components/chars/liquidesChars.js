6deccee0637a4ebb368da0a100fe16faabb61afa
msn numerique <ndourm9@gmail.com>
2026-09-05 15:46:00 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -344,7 +344,8 @@ export default function Charts(){
   );
 };
 
-const BarComponent({dp,vl,prctge})=()=>{
+const BarComponent({params})=()=>{
+  const {dp,vl,prctge}=params;
   return <View style={{
                 position: 'absolute',
                 justifyContent: 'center',
