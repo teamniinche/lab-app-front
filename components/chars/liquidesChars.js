f72c28d116828d740c2511737edf1d1a4b5897fb
msn numerique <ndourm9@gmail.com>
2026-09-06 21:42:08 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -422,10 +422,10 @@ export default function BarsChart(){
             labelComponent:()=>(<Text style={{backgroundColor:'rgba(0,0,0,0.3)',color:'white',fontWeight:'bold',letterSpacing:1.5}}>{reduceText(key)}</Text>),
 
             // 🎯 Solution : Affiche le nombre 'len' de manière centrée tout en haut de la barre
-            topLabelComponent:()=>(<Text style={{flexDirection:'column',backgroundColor:'rgba(0,0,250,0.4)',textAlign:'center',color:'white',borderRadius:5,padding:5,paddingHorizontal:8,minWidth:40,width:'auto',height:'auto'}}>
-              <Text style={{color:'white'}}>{((len/analyses?.length)*100).toFixed(1).toString()+'%'}</Text>
-              <Text style={{color:'white'}}>{len}</Text>
-              </Text>),
+            topLabelComponent:()=>(<View style={{flexDirection:'column',justifyContent:'center',alignItems:'center',backgroundColor:'rgba(0,0,250,0.4)',borderRadius:5,padding:5,paddingHorizontal:8,minWidth:40,width:'auto',height:'auto'}}>
+              <Text style={{textAlign:'center',color:'white'}}>{((len/analyses?.length)*100).toFixed(1).toString()+'%'}</Text>
+              <Text style={{textAlign:'center',color:'white'}}>{len}</Text>
+              </View>),
             frontColor:clor
             // innerBarComponent: () => (<BarsComponent dp={name} />)
           }
