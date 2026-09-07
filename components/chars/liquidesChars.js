58d2ed933d49725cbb13ce24b792d260d8008a94
msn numerique <ndourm9@gmail.com>
2026-09-07 11:32:02 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -82,23 +82,24 @@ const GraphesNav=({render})=>{
     }
     const hoverStyle={backgroundColor:'rgba(0,0,250,0.1)',borderRadius:4}
     const focusStyle={backgroundColor:'rgba(0,0,250,0.2)',borderRadius:4}
-    return <View style={{flexDirection:'row',justifyContent:'flex-start',gap:5,alignItems:'center',minWidth:1050,heigth:40,paddingHorizontal:10,paddingVertical:5,marginHorizontal:'auto',borderRadius:5,borderWidth:1,borderBottomWidth:0,borderColor:'grey',backgroundColor:'rgba(240,240,240,0.2)'}}>
-        <Text style={{color:primaryColor,backgroundColor:'rgba(0,0,0,0.15)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,fontWeight:'bold'}}>Graphiques</Text>
-        {Object.entries(graphes).map(([k,value])=>{
-          const {component,info}=value;
-          return <Btn
-              style={[{width:'100%',height:40,padding:5},grap===k?hoverStyle:{},focusedGrap===k?focusStyle:{}]} 
-              onPress={()=>handleGrapPress(component,k)}
-              onHoverIn={()=>setGrap(k)}
-              onHoverOut={()=>setGrap(null)}
-              bcgrndClr={'rgba(0,0,0,0.8)'}
-              textStyle={styles.tooltipText}
-              info={info}
-
-          >
-            <Text style={{color:focusedGrap===k?'white':'black',letterSpacing:2,fontSize:13,fontWeight:'bold'}}>{k.replace('ProductionsPar',' / ')}</Text>
-          </Btn>
-        })}
+    return <View style={{flexDirection:'column',justifyContent:'flex-start',alignItems:'flex-start',gap:3,minWidth:1050,heigth:'auto',paddingHorizontal:10,paddingVertical:4,borderRadius:5,borderWidth:1,borderRightWidth:0,borderColor:'grey',backgroundColor:'rgba(240,240,240,0.2)'}}>
+        <Text style={{color:primaryColor,height:20,backgroundColor:'rgba(0,0,0,0.15)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,fontWeight:'bold'}}>Graphiques</Text>
+        <View style={{flexDirection:'row',justifyContent:'flex-start',gap:5,alignItems:'center',width:'auto',height:30}}>
+            {Object.entries(graphes).map(([k,value])=>{
+              const {component,info}=value;
+              return <Btn
+                  style={[{width:'100%',height:25,padding:2},grap===k?hoverStyle:{},focusedGrap===k?focusStyle:{}]} 
+                  onPress={()=>handleGrapPress(component,k)}
+                  onHoverIn={()=>setGrap(k)}
+                  onHoverOut={()=>setGrap(null)}
+                  bcgrndClr={'rgba(0,0,0,0.8)'}
+                  textStyle={styles.tooltipText}
+                  info={info}
+              >
+                <Text style={{color:focusedGrap===k?'white':'black',letterSpacing:2,fontSize:13,fontWeight:'bold'}}>{k.replace('ProductionsPar','Prods/')}</Text>
+              </Btn>
+            })}
+        </View>
       </View>
   }
 
