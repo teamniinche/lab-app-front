8717abed1003fdce79b5f9bb7c5de4d4932b5c95
msn numerique <ndourm9@gmail.com>
2026-09-09 19:01:52 +0200

    'commitauto'

--- assets/colors.js
+++ assets/colors.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Colors = {
     Citron: "#b8ff05",//"#86c727",
     Platinium: "#ff5f03",//"#fdc412",//#f3691d
@@ -52,59 +51,4 @@ const Colors = {
     }
   }
 
-=======
-const Colors = {
-    Citron: "#b8ff05",//"#86c727",
-    Platinium: "#ff5f03",//"#fdc412",//#f3691d
-    Premium: "#8103ff",//"#6f08bf",
-    Incolore: "whitesmoke",
-    Blanc: "white",
-    Bleu: "#0a9fc4",//"#193bd8",
-    Rouge: "#d20606",
-    Noir:"black",
-    Marron: "#c76927",
-    Jaune: "yellow",
-    Alerte:"#e61e18",
-    Valide:"#088310",
-    Rose:"pink",
-    Fraise:"red",
-    Pomme:"#b8ff05",
-    Flamboise:"#a31234",
-    Lavander:'#7312a3',
-    Fama:'#0bbd3b',
-    Marseille:"whitesmoke",
-    Fruit:"#c90620",
-    Vanille:'#ad4728',
-    Passion:'#eb7413',
-    Gettol:'#8c3109',
-    Javel:'#7a630564',
-    rootColor:'#6200ee',
-  };
-
-  export const themes={
-    light:{
-      icone:'sun-o',
-      iconeColor:'white',
-      backgroundClr:'black',
-      mainColor:'rgba(255,255,255,0.9)',
-      transparent:'transparent',
-      mainTextColor:'black',
-      lessTextColor:'rgba(0,0,0,0.8)',
-      worstTextColor:'rgba(0,0,0,0.1)',
-      mediumTextColor:'grey'
-    },
-    dark:{
-      icone:'moon-o',
-      iconeColor:'black',
-      backgroundClr:'white',
-      mainColor:'rgba(0,0,0,0.9)',
-      transparent:'transparent',
-      mainTextColor:'white',
-      lessTextColor:'rgba(255,255,255,0.8)',
-      worstTextColor:'rgba(250,250,250,0.4)',
-      mediumTextColor:'rgba(250,250,250,0.6)'
-    }
-  }
-
->>>>>>> bd44746db4e86c8bf933e9c470714a63e7e70465
   export default Colors;
