1bc1bf4c474160082ead41f3bd9c403c1ca758da
msn numerique <ndourm9@gmail.com>
2026-09-09 19:19:55 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -29,7 +29,8 @@ const LateralNav=({pstdAnalyses,render})=>{
     const focusStyle={backgroundColor:'rgba(0,0,250,0.3)'};
     const hoverStyle={backgroundColor:'rgba(0,0,250,0.08)'};
 return <View style={{width:200,minHeigth:500,paddingHorizontal:10,paddingVertical:20,paddingTop:5,marginRight:15,borderRadius:5,borderWidth:1,borderBottomWidth:0,borderColor:'grey',backgroundColor:'whitesmoke'}}>
-        <Text style={{color:primaryColor,backgroundColor:'rgba(0,0,0,0.15)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,fontWeight:'bold'}}>Départements</Text>
+        {/* <Text style={{color:primaryColor,backgroundColor:'rgba(0,0,0,0.15)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,fontWeight:'bold'}}>Départements</Text> */}
+        <Text style={{color:'grey',backgroundColor:'rgba(0,0,0,0.07)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,borderWidth:1,brderColor:'rgba(0,0,0,0.05)'}}>Départements</Text>
         {Object.entries(prodsByDep).sort((a,b)=>b[0].localeCompare(a[0])).map(([k,items])=>{
           return <Pressable 
               style={[{width:'100%',height:50,padding:5,borderRadius:4},dep===k && hoverStyle,focusedDep===k && focusStyle]} 
