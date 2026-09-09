7eebbee75e16fc2ca80533095dbcdf84720dce10
msn numerique <ndourm9@gmail.com>
2026-09-09 15:32:18 +0200

    'commitauto'

--- components/chars/liquidesChars.js
+++ components/chars/liquidesChars.js
@@ -21,20 +21,20 @@ const Texts=({text,focusedDep})=>{
   }
 const LateralNav=({pstdAnalyses,render})=>{
     const prodsByDep=filter.filterByDep(pstdAnalyses);
-    const [dep,setDep]=useState(Object.keys(prodsByDep)[0]);
-    const [focusedDep,setFocusedDep]=useState(null);
+    const [dep,setDep]=useState(Object.keys(prodsByDep)[0] || null);
+    const [focusedDep,setFocusedDep]=useState(Object.keys(prodsByDep)[0] || null);
     function handleDepPress(items,k){
       render(items);
       setFocusedDep(k);
       setDep(k);
     }
-    const hoverStyle={backgroundColor:'rgba(0,0,250,0.1)',borderRadius:4}
-    const focusStyle={backgroundColor:'rgba(0,0,250,0.2)',borderRadius:4}
+    const focusStyle={backgroundColor:'rgba(0,0,250,0.3)'};
+    const hoverStyle={backgroundColor:'rgba(0,0,250,0.1)'};
     return <View style={{width:200,minHeigth:500,paddingHorizontal:10,paddingVertical:20,paddingTop:5,marginRight:15,borderRadius:5,borderWidth:1,borderBottomWidth:0,borderColor:'grey',backgroundColor:'whitesmoke'}}>
         <Text style={{color:primaryColor,backgroundColor:'rgba(0,0,0,0.15)',borderRadius:4,paddingVertical:20,textAlign:'center',marginBottom:20,letterSpacing:2,fontSize:14,fontWeight:'bold'}}>Départements</Text>
         {Object.entries(prodsByDep).sort((a,b)=>b[0].localeCompare(a[0])).map(([k,items])=>{
           return <Pressable 
-              style={[{width:'100%',height:50,padding:5},dep===k && hoverStyle,focusedDep===k && focusStyle]} 
+              style={[{width:'100%',height:50,padding:5,borderRadius:4},dep===k && hoverStyle,focusedDep===k && focusStyle]} 
               onPress={()=>handleDepPress(items,k)}
               onHoverIn={()=>setDep(k)}
               onHoverOut={()=>setDep(null)}
