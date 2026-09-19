cda451eec3184a559f7d9b91041154f81761e441
msn numerique <ndourm9@gmail.com>
2026-09-19 14:39:00 +0200

    'commitauto'

--- components/chars/poudresChars.js
+++ components/chars/poudresChars.js
@@ -483,7 +483,7 @@ function PieCharts({powderAnalysed}) {
            // 2. On retourne TOUJOURS un objet valide avec une valeur numérique
     return { 
       value: Number(prctge.toFixed(1)), // 🟢 S'assure que c'est un nombre pur
-      text: `${Replace(key,[['sans sel','ssl'],['avec sel','asl'], ['export','exp']])} (${prctge.toFixed(1)}%)`, // 🟢 Le .toFixed sert uniquement pour le texte d'affichage
+      text: `${Replace(key,[['sans sel','ssl'],['avec sel','asl'], ['export','exp']])}\n(${prctge.toFixed(1)}%)`, // 🟢 Le .toFixed sert uniquement pour le texte d'affichage
       color: fullPalette[index], // 🟢 Sécurité avec le modulo %
       // labelComponent: () => (
       //   <View style={{width:'auto',minWidth:100,flexDirection:'column',alignItems: 'center', justifyContent: 'center',backgroundColor:'white',borderRadius:10,paddingHorizontal:4}}>
